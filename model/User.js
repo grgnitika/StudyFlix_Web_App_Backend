b1cd883de7445bba20fbb528d1f95dd19b0f9aeb
grgnitika <gurungnitika59@gmail.com>
2025-01-08 19:26:27 +0545

    user model created

---
+++ model/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const userSchema= new mongoose.Schema({
+    full_name: {
+        type: String,
+        reqired: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    contact_no: {
+        type: String,
+        required: true
+    },
+})
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
